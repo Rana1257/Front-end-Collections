5f736d474fc48ab0cf4a3eb0d124ed050d2d453d
Rana1257 <953924751@qq.com>
2020-05-04 17:17:37 +0800

    loading

--- src/main.js
+++ src/main.js
@@ -3,13 +3,25 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import VueJsonp from 'vue-jsonp'
+import Button from 'element-ui/lib/button'
+import Main from 'element-ui/lib/main'
+import Row from 'element-ui/lib/row'
+import Col from 'element-ui/lib/col'
+import Menu from 'element-ui/lib/menu'
+import MenuItem from 'element-ui/lib/menu-item'
+import Backtop from 'element-ui/lib/backtop'
 
 Vue.use(VueJsonp)
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(Button)
+Vue.use(Main)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Backtop)
 
 /* eslint-disable no-new */
 new Vue({
